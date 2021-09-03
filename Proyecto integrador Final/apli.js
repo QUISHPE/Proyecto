34e9570087ea748568e34c250c15e88e29f71ed2
QUISHPE <86366639+QUISHPE@users.noreply.github.com>
2021-09-03 12:24:03 -0500

    Add files via upload

---
+++ Proyecto integrador Final/apli.js
@@ -0,0 +1,42 @@
+function temperatura35() {
+    
+    alert("Hipotermia: La hipotermia es una urgencia médica que ocurre cuando el cuerpo pierde calor más rápido de lo que lo produce, lo que provoca una peligrosa disminución de la temperatura corporal.")
+    alert("AUTOTRATAMIENTO: Quitarse la ropa mojada, envolver el cuerpo con frazadas, tomar bebidas calientes y darse un baño caliente puede ayudar a elevar la temperatura corporal.")
+}
+function temperatura36() {
+    
+    alert("Una temperatura corporal de 36 - 36,1 - 36,2 es prácticamente normal y no debería constituir ningún riesgo para la salud.")
+}
+function temperatura37() {
+    alert("Fiebre: Aumento temporal de la temperatura corporal promedio, que suele ser de 37 °C (98.6 °F).")
+    alert("La fiebre no es una enfermedad. Por lo general, es una señal de que su cuerpo está tratando de combatir una enfermedad o infección.")
+    alert("AUTOTRATAMIENTO: Tomar medicamentos como el ibuprofeno y el paracetamol puede aliviar la molestia. Evita administrar aspirina a los niños, ya que puede causar un trastorno poco frecuente, pero grave.")
+}
+function temperatura39() {
+    
+    alert("Fiebre de los tres días: Infección viral común en los niños pequeños que puede ocasionar fiebre alta y sarpullido.")
+    alert("El tratamiento consiste en analgésicos y en hacer reposo, ingerir líquidos y tomar medicamentos para reducir la fiebre.")
+}
+function temperatura41() {
+    
+    alert("Hipertermia: Temperatura corporal elevada a niveles peligrosos, generalmente como consecuencia de un clima cálido y húmedo por tiempo prolongado.")
+    alert("La hipertermia ocurre cuando la temperatura corporal asciende a niveles superiores a los normales y el sistema de termorregulación del cuerpo no puede funcionar correctamente. En este punto, el cuerpo no puede enfriarse solo, condición que puede llegar a ser mortal si no se trata.")
+    alert("El tratamiento consiste en el cuidado personal: Es fundamental mover a una persona con hipertermia a un lugar fresco y buscar atención médica de emergencia.")
+}
+function bajoPeso(){
+    alert("Tener un peso que se considera demasiado bajo para gozar de buena salud. Esto sucede cuando es debido a una mal nutrición, hay un embarazo o por otros problemas de salud.")
+    alert("El bajo peso puede ser el síntoma de una patología subyacente. En caso de una pérdida de peso inexplicada, es fundamental realizar un diagnóstico médico.")
+    alert("Por otro lado, el peso bajo puede ser una condición causal primaria. Las personas con esta condición pueden tener un sistema inmunológico débil, así como una pobre condición física, haciéndolos propensos a padecer infecciones.")
+    alert("Tratamiento: Si lo indica un médico, comer frecuentemente comidas con muchos nutrientes puede ayudar a aumentar de peso. Realizar ejercicio moderado también puede ayudar a aumentar la masa muscular y estimular el apetito.")
+}
+function normalP(){
+    alert("Se considera un peso saludable aquel que nos permite mantenernos en un buen estado de salud y calidad de vida. También se entiende como los valores de peso, dentro de los cuales, no existe riesgo para la salud de la persona.")
+}
+function sobrePeso(){
+    alert("Sobrepeso: Peso por encima de lo que se considera saludable, a menudo se mide mediante el índice de masa corporal (IMC).")
+    alert("Tratamiento: Consumir menos calorías, elegir los alimentos más saludables, realizar más ejercicio y controlar el peso puede ayudar a reducir la masa corporal.")
+    alert("Buscar atencion medica: consulta a tu medico lo siguiente")
+}
+function Obeso(){
+    alert("Obesidad: Trastorno caracterizado por niveles excesivos de grasa corporal que aumentan el riesgo de tener problemas de salud.")
+}
